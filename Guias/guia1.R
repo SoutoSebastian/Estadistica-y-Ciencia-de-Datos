b80a3adb782ad82e7141131f63b7e248a461d9f9
Sebastian <soutosebastianma@gmail.com>
2025-08-20 11:33:42 -0300

    ej 4 guia 1

--- Guias/guia1.R
+++ Guias/guia1.R
@@ -155,3 +155,129 @@ madRodio <- mad(rodio[,1])
 cuantilesIridio <- quantile(iridio[,1], probs = c(0.9, 0.75, 0.5, 0.2, 0.1))
 
 cuantilesRodio <- quantile(rodio[,1], probs = c(0.9, 0.75, 0.5, 0.2, 0.1))
+
+
+####Ejercicio 4#####
+
+###a
+
+salchichasA <- read.table("salchichas_A.txt", header = TRUE)
+salchichasB <- read.table("salchichas_B.txt", header = TRUE)
+salchichasC <- read.table("salchichas_C.txt", header = TRUE)
+
+
+salchichasA$tipo <- "A"
+salchichasB$tipo <- "B"
+salchichasC$tipo <- "C"
+
+names(salchichasA)[names(salchichasA) == "CALORIAS.A"]<- "CALORIAS"
+names(salchichasB)[names(salchichasB) == "CALORIAS.B"]<- "CALORIAS"
+names(salchichasC)[names(salchichasC) == "CALORIAS.C"]<- "CALORIAS"
+
+names(salchichasA)[names(salchichasA) == "SODIO.A"]<- "SODIO"
+names(salchichasB)[names(salchichasB) == "SODIO.B"]<- "SODIO"
+names(salchichasC)[names(salchichasC) == "SODIO.C"]<- "SODIO"
+
+salchichas <- rbind(salchichasA, salchichasB, salchichasC)
+
+###b
+
+#Histograma A
+
+hist(salchichasA[,1], 
+     main = "Calorias salchichas A",  
+     xlab = "Calorias",         
+     ylab = "Frecuencia",             
+     col = rgb(0,0,1,0.5),                  
+     border = "black",                 
+     breaks = 10,                      
+     freq = TRUE)
+
+#Histograma B
+
+hist(salchichasB[,1], 
+     main = "Calorias salchichas B",  
+     xlab = "Calorias",         
+     ylab = "Frecuencia",             
+     col = "skyblue",                  
+     border = "black",                 
+     breaks = 10,                      
+     freq = TRUE)
+
+#Histograma C
+
+hist(salchichasC[,1], 
+     main = "Calorias salchichas C",  
+     xlab = "Calorias",         
+     ylab = "Frecuencia",             
+     col = "salmon",                  
+     border = "black",                 
+     breaks = 10,                      
+     freq = TRUE)
+
+
+
+#En los graficos A y B se observan grupos. Se podria considerar que hay dos grupos.
+#Se observan candidatos a datos atípicos en los gráficos A y B. El histograma C tiene
+#observaciones a lo largo de la recta. 
+
+###c
+
+boxplot(salchichasA[,1], salchichasB[,1],salchichasC[,1],
+        names = c("A", "B","C"),
+        col = c(rgb(0,0,1,0.5),"skyblue", "salmon"),
+        main = "Calorías en salchichas de distintos tipos.",
+        ylab = "Calorias")
+
+
+#Como se observan grupos en un boxplot? ayuda para el analisis.
+
+
+###d 
+
+#Sodio
+
+#Histograma A
+
+hist(salchichasA[,2], 
+     main = "Sodio salchichas A",  
+     xlab = "Sodio",         
+     ylab = "Frecuencia",             
+     col = rgb(0,0,1,0.5),                  
+     border = "black",                 
+     breaks = 10,                      
+     freq = TRUE)
+
+#Histograma B
+
+hist(salchichasB[,2], 
+     main = "Sodio salchichas B",  
+     xlab = "Sodio",         
+     ylab = "Frecuencia",             
+     col = "skyblue",                  
+     border = "black",                 
+     breaks = 10,                      
+     freq = TRUE)
+
+#Histograma C
+
+hist(salchichasC[,2], 
+     main = "Sodio salchichas C",  
+     xlab = "Sodio",         
+     ylab = "Frecuencia",             
+     col = "salmon",                  
+     border = "black",                 
+     breaks = 10,                      
+     freq = TRUE)
+
+
+#Analisis
+
+#Boxplot
+
+boxplot(salchichasA[,2], salchichasB[,2],salchichasC[,2],
+        names = c("A", "B","C"),
+        col = c(rgb(0,0,1,0.5),"skyblue", "salmon"),
+        main = "Sodio en salchichas de distintos tipos.",
+        ylab = "Sodio")
+
