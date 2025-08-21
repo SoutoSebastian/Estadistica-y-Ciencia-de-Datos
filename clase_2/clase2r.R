a5be45b7570583fc0f3376f088c6ddb79af59200
SoutoSebastian <142252739+SoutoSebastian@users.noreply.github.com>
2025-08-21 20:44:40 -0300

    Clase 2

---
+++ clase_2/clase2r.R
@@ -0,0 +1,63 @@
+######Clase 2###########
+
+
+###Ej 1###
+
+datos_bebes <- read.table("ENNyS_menorA2.txt", header = TRUE)
+
+#a
+
+#Sexo.
+table_abs_Sexo <- table(datos_bebes$Sexo)
+table_rel_Sexo <- prop.table(table_abs_Sexo)
+
+#Tipo de embarazo.
+table_abs_Embarazo <- table(datos_bebes$Tipo_embarazo)
+table_rel_Embarazo <- prop.table(table_abs_Embarazo)
+
+#b GRAFICO DE BARRAS
+
+#Sexo
+
+barplot(table_rel_Sexo,
+        main = "Frecuencia relativa Sexo",
+        xlab = "Sexo",
+        ylab = "Frecuencia",
+        col = c("pink", "lightblue")) 
+
+#Tipo de embarazo
+
+barplot(table_rel_Embarazo,
+        main = "Frecuencia relativa tipo de embarazo",
+        xlab = "Tipo de embarazo",
+        ylab = "Frecuencia",
+        col = c("pink", "lightblue")) 
+
+
+#c GRAFICO DE TORTAS
+
+#Sexo
+
+pie(table_rel_Sexo, 
+    main = "Frecuencia Géneros", 
+    col = c("pink", "lightblue"))
+
+
+#Tipo de embarazo
+
+
+pie(table_rel_Embarazo, 
+    main = "Frecuencia tipo de embarazo", 
+    col = c("pink", "lightblue"))
+
+
+
+###Ej 2###
+
+cont_SexoEmbarazo <- table(datos_bebes$Sexo, datos_bebes$Tipo_embarazo)
+
+barplot(cont_SexoEmbarazo[1,],
+        main = "Cantidad",
+        xlab = "Tipo de embarazo en mujeres",
+        ylab = "Frecuencia",
+        col = "pink") 
