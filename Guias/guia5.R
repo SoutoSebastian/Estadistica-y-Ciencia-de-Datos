6332ce1824fdf8fa33785fb3621f55b205f7e603
SoutoSebastian <soutosebastianma@gmail.com>
2025-11-02 22:15:05 -0300

    cuestionario

--- Guias/guia5.R
+++ Guias/guia5.R
@@ -160,3 +160,191 @@ s <- sd(grasa)
 estadistico <- sqrt(12) *(promedio - 18)/s
 
 
+######Ej 9######
+
+#e
+
+# Parámetros
+n <- 50
+p0 <- 0.3
+alpha <- 0.05
+z <- qnorm(1 - alpha/2)
+
+# Vector de valores verdaderos de p
+p <- seq(0.05, 0.95, by = 0.001)
+
+# Funciones de potencia
+
+
+pi_T <- 1 - pnorm(z - sqrt(n)*(p - p0)/sqrt(p*(1-p))) +
+  pnorm(-z - sqrt(n)*(p - p0)/sqrt(p*(1-p)))
+
+# Gráfico
+plot(p, pi_W, type = "l", col = "blue", lwd = 2,
+     ylim = c(0,1), xlab = "p verdadero", ylab = "Potencia aproximada",
+     main = "Funciones de potencia aproximadas (n = 50, p0 = 0.3)")
+lines(p, pi_T, col = "red", lwd = 2, lty = 2)
+abline(v = p0, lty = 3)
+legend("bottomright", legend = c("Test clásico (W)", "Test plug-in (T)"),
+       col = c("blue", "red"), lwd = 2, lty = c(1,2))
+
+
+
+#f
+
+
+set.seed(123)  # reproducibilidad
+
+n <- 50
+p0 <- 0.3
+alpha <- 0.05
+z <- qnorm(1 - alpha/2)
+p_vals <- c(0.03, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.97)
+B <- 100000  # cantidad de simulaciones
+
+# vectores donde guardamos las potencias
+pi_W <- numeric(length(p_vals))
+pi_T <- numeric(length(p_vals))
+
+for (i in seq_along(p_vals)) {
+  p <- p_vals[i]
+  
+  # simulamos muestras de tamaño n, repetidas B veces
+  Xbar <- rbinom(B, n, p) / n
+  
+  # estadísticos
+  W <- sqrt(n) * (Xbar - p0) / sqrt(p0*(1 - p0))
+  T <- sqrt(n) * (Xbar - p0) / sqrt(Xbar*(1 - Xbar))
+  
+  # rechazos
+  pi_W[i] <- mean(abs(W) > z)
+  pi_T[i] <- mean(abs(T) > z)
+}
+
+# mostramos resultados
+res <- data.frame(p = p_vals, potencia_W = pi_W, potencia_T = pi_T)
+print(res)
+
+# gráfico interpolado
+plot(p_vals, pi_W, type = "o", pch = 16, col = "blue", ylim = c(0,1),
+     xlab = "p verdadero", ylab = "Potencia estimada",
+     main = "Funciones de potencia simuladas (n = 50, p0 = 0.3)")
+lines(p_vals, pi_T, type = "o", pch = 17, col = "red", lty = 2)
+abline(v = p0, lty = 3)
+legend("bottomright", legend = c("Test clásico (W)", "Test plug-in (T)"),
+       col = c("blue", "red"), lwd = 2, pch = c(16,17), lty = c(1,2))
+
+
+######EJ 12#######
+
+#a
+set.seed(2025)
+
+Nsim <- 200000
+n1 <- 20
+n2 <- 10
+alpha <- 0.05
+z <- qnorm(1 - alpha)   # percentil 0.95, umbral unilateral
+
+rejects <- 0L
+
+for (i in 1:Nsim) {
+  x1 <- rnorm(n1, mean = 0, sd = 1)      # bajo H0: mu = 0
+  T1 <- sqrt(n1) * mean(x1)
+  if (T1 > z) {
+    rejects <- rejects + 1
+  } else {
+    x2 <- rnorm(n2, mean = 0, sd = 1)
+    x_all <- c(x1, x2)
+    T30 <- sqrt(n1 + n2) * mean(x_all)
+    if (T30 > z) rejects <- rejects + 1
+  }
+}
+
+p_hat <- rejects / Nsim
+cat("Rechazo empírico (nivel real):", p_hat, "\n")
+
+#El nivel queda aproximandamente 0.07.
+
+
+#b
+
+Nsim <- 200000
+n1 <- 20
+n2 <- 10
+alpha <- 0.05
+z <- qnorm(1 - alpha)   # percentil 0.95, umbral unilateral
+mus <- c(0.5, 1, 2, 5, 10)
+potencias <- 0
+
+
+for (j in 1:length(mus)){
+  rejects <- 0L
+  for (i in 1:Nsim) {
+    x1 <- rnorm(n1, mean = mus[j], sd = 1)      # bajo H0: mu = 0
+    T1 <- sqrt(n1) * mean(x1)
+    if (T1 > z) {
+      rejects <- rejects + 1
+    } else {
+      x2 <- rnorm(n2, mean = mus[j], sd = 1)
+      x_all <- c(x1, x2)
+      T30 <- sqrt(n1 + n2) * mean(x_all)
+      if (T30 > z) rejects <- rejects + 1
+    }
+  }
+  
+  potencias[j] <- rejects / Nsim
+
+}
+
+
+########EJ 16#########
+
+datos_x <- c(25, 19.5, 16.6, 21.3, 20.7, 16.8)
+datos_y <- c(23.8, 19, 15.9, 20.4, 19.6, 15.8)
+
+datosD <- datos_x - datos_y
+
+
+
+library(ggplot2)
+
+datos <- data.frame(val = datosD)
+
+ggplot(datos, aes(sample = val)) +
+  stat_qq() +
+  stat_qq_line() +
+  ggtitle("QQ-Plot de datos_x")
+
+
+
+D_raya <- mean(datosD)
+S <- sd(datosD)
+
+
+#######Cuestionario##########
+
+datos <- c(24, 21, 27, 28, 23, 22, 36, 24, 33, 14, 25)
+
+S2 <- var(datos)
+
+U_obs <- 10*S2/49
+
+p_val <- pchisq(U_obs,10)
+
+
+sa <- 2.76
+sb <- 1.92
+xa <- 6.84
+xb <- 7.81
+na <- 55
+nb <- 57
+
+sp2 <- ((2.76)^2 * 54 + (1.92^2)*56) / (55+57-2)
+
+Tobs <- abs(xa - xb)/(sqrt(sp2*(1/na + 1/nb)))
+pval <- 2 * pt(Tobs, 110, lower.tail = FALSE)
+
+limInf<- xa - xb - qt(0.005, 110, lower.tail = FALSE) * sqrt(sp2 *(1/na + 1/nb))
+limSup<- xa - xb + qt(0.005, 110, lower.tail = FALSE) * sqrt(sp2 *(1/na + 1/nb)) 
+
